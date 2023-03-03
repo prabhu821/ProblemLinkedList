a37050bf0c2c283c3133193e6561a93053e8ae9c
rishiraj007 <rishiraj@mounttalent.com>
2023-03-03 19:30:32 +0530

    [Prabhu] Add: Linked List Stack Operations

---
+++ ProblemLinkedList/LinkedListStack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemLinkedList;
+
+public class LinkedListStack
+{
+    private Node top;
+    public LinkedListStack()
+    {
+        this.top = null;
+    }
+    public void Push(int value)
+    {
+        Node node = new Node(value);
+        if (this.top == null)
+        {
+            node.next = null;
+        }
+        else
+        {
+            node.next = this.top;
+        }
+        this.top = node;
+        Console.WriteLine("{0} pushed to stack", value);
+    }
+    public void Display()
+    {
+        Node temp = this.top;
+        while (temp != null)
+        {
+            Console.Write(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    public void Peek()
+    {
+        if (this.top == null)
+        {
+            Console.WriteLine("Stack is empty");
+            return;
+        }
+        Console.WriteLine("{0} is in the top of the stack", this.top.data);
+    }
+    public void Pop()
+    {
+        if (this.top == null)
+        {
+            Console.WriteLine("Stack is empty, Deleting is not possible");
+            return;
+        }
+        Console.WriteLine("Value popped is {0}", this.top.data);
+        this.top = this.top.next;
+    }
+    public void IsEmpty()
+    {
+        while (this.top != null)
+        {
+            Peek();
+            Pop();
+        }
+    }
+}
--- ProblemLinkedList/Program.cs
+++ ProblemLinkedList/Program.cs
@@ -5,16 +5,25 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Linked List Problem!");
-        LinkedList list = new LinkedList();
-        list.Add(50);
-        list.Add(70);
-        list.Add(30);
-        list.Display();
-        int a = list.Search(30);
-        list.InsertAtParticularPosition(a + 1, 40);
-        list.Display();
-        list.DeleteNodeAtParticularPosition(2);
-        list.Size();
-        list.Display();
+        //LinkedList list = new LinkedList();
+        //list.Add(50);
+        //list.Add(70);
+        //list.Add(30);
+        //list.Display();
+        //int a = list.Search(30);
+        //list.InsertAtParticularPosition(a + 1, 40);
+        //list.Display();
+        //list.DeleteNodeAtParticularPosition(2);
+        //list.Size();
+        //list.Display();
+
+        LinkedListStack stack = new LinkedListStack();
+        stack.Push(50);
+        stack.Push(70);
+        stack.Push(30);
+        stack.Display();
+        stack.Peek();
+        stack.Pop();
+        stack.Display();
     }
 }
