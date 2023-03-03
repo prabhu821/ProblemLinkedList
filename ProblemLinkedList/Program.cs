40ff0bb487907eaeea5eda50c294757fec786d6c
rishiraj007 <rishiraj@mounttalent.com>
2023-03-03 19:19:05 +0530

    [Prabhu] Add: Linked List Add Data

--- ProblemLinkedList/Program.cs
+++ ProblemLinkedList/Program.cs
@@ -5,5 +5,10 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Linked List Problem!");
+        LinkedList list = new LinkedList();
+        list.Add(50);
+        list.Add(70);
+        list.Add(30);
+        list.Display();
     }
 }
